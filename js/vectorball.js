a857802370449b6ddf173a589aa3f02c012a2727
sam <sam.w.mackinnon@gmail.com>
2017-02-05 00:05:42 -0800

    vectorball updates

--- js/vectorball.js
+++ js/vectorball.js
@@ -1,11 +1,28 @@
-var game = new Phaser.Game(600, 500, Phaser.CANVAS, 'vb-stage', { preload: preload, create: create, update: update, render: render }, true);
+var game = new Phaser.Game(550, 500, Phaser.CANVAS, 'vb-stage', { preload: preload, create: create, update: update, render: render }, true);
 var levels = [
+    {
+        start: { x: 0, y: 400 },
+        finish: { x: 500, y: 400 },
+        map: [
+            ["F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+            ["F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+            ["R", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+            ["F", "X", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+            ["F", "X", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+            ["F", "X", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+            ["F", "X", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+            ["F", "X", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+            ["F", "X", "F", "F", "F", "F", "F", "F", "F", "F", "F"]
+        ],
+        tiles: ["N", "W", "E", "S", "N", "W", "E", "S", "N", "W", "E"],
+        balls: [{ x: 0, y: 200 }]
+    },
     {
         start: { x: 0, y: 200 },
         finish: { x: 500, y: 200 },
         map: [
             ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
-            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "R", "X", "X", "X", "X", "X"],
             ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
             ["F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
             ["F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
@@ -14,23 +31,42 @@ var levels = [
             ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
             ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"]
         ],
-        tiles: ["E"]
+        tiles: ["E"],
+        balls: [{ x: 0, y: 200 }]
     },
     {
-        start: { x: 50, y: 400 },
-        finish: { x: 450, y: 200 },
+        start: { x: 0, y: 400 },
+        finish: { x: 500, y: 150 },
         map: [
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
             ["F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
             ["F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
             ["F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
-            ["F", "F", "F", "X", "X", "X", "X", "X", "F", "F", "F"],
-            ["F", "F", "F", "X", "X", "X", "X", "X", "F", "F", "F"],
-            ["F", "F", "F", "X", "X", "X", "X", "X", "F", "F", "F"],
-            ["F", "F", "F", "X", "X", "X", "X", "X", "F", "F", "F"],
-            ["F", "F", "F", "X", "X", "X", "X", "X", "F", "F", "F"],
-            ["F", "F", "F", "X", "X", "X", "X", "X", "F", "F", "F"]
+            ["F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+            ["F", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["F", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"]
         ],
-        tiles: ["N", "E", "S", "S"]
+        tiles: ["N", "E", "E", "E", "E"],
+        balls: [{ x: 0, y: 200 }]
+    },
+    {
+        start: { x: 300, y: 250 },
+        finish: { x: 250, y: 200 },
+        map: [
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "F", "F", "F", "F", "F", "F", "F", "X", "X"],
+            ["X", "F", "F", "F", "F", "F", "F", "F", "F", "F", "X"],
+            ["X", "F", "F", "F", "F", "F", "F", "F", "F", "F", "X"],
+            ["X", "F", "F", "F", "F", "F", "F", "F", "F", "F", "X"],
+            ["X", "F", "F", "F", "F", "F", "F", "F", "F", "F", "X"],
+            ["X", "F", "F", "F", "F", "F", "F", "F", "F", "F", "X"],
+            ["X", "X", "F", "F", "F", "F", "F", "F", "F", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"]
+        ],
+        tiles: ["N", "W", "E", "S", "N", "W", "S"],
+        balls: [{ x: 0, y: 200 }]
     },
     {
         start: { x: 0, y: 400 },
@@ -46,9 +82,16 @@ var levels = [
             ["F", "F", "X", "X", "F", "F", "F", "F", "F", "F", "F"],
             ["F", "F", "X", "X", "F", "F", "F", "F", "F", "F", "F"]
         ],
-        tiles: ["N", "N", "N","W","W","S","S","E"]
+        tiles: ["N", "N", "N", "W", "W", "S", "S", "E"],
+        balls: [{ x: 250, y: 250 }]
     }
 ]
+var levelEnd = false;
+var doors = [];
+var doorAngle = 0;
+var nope;
+var currentTile = null;
+var go = true;
 var lvlnum = 0;
 var level = levels[lvlnum];
 var map = levels[lvlnum].map;
@@ -56,40 +99,79 @@ var tileWidth = 50;
 var levelWin = false;
 var ball;
 var speed = 200;
+var acc = 200;
 var tiles2 = [];
-var tiles;
+var tiles1 = [];
 var end;
 var dy = -1;
 var dx = 0;
+var collideTimer = null;
 var startx = level.start.x;
 var starty = level.start.y;
 var finx = level.finish.x;
 var finy = level.finish.y;
 var graphics;
+var line;
+var lineGrp;
 var holes = [];
 var nodes = [];
 var plus;
+var nextbtn;
+var complete;
 function preload() {
+    game.load.spritesheet('G', 'assets/vectorball/adown.png', 50, 50);
+    game.load.spritesheet("R", 'assets/vectorball/staticright.png', 50, 50);
+    game.load.spritesheet('next', 'assets/vectorball/nextbtn.png', 150, 50);
+    game.load.image("nope", 'assets/vectorball/nope.png', 50, 50);
     game.load.image('B', 'assets/vectorball/ball.png', 50, 50);
     game.load.image('F', 'assets/vectorball/floor.png', 50, 50);
-    game.load.spritesheet('E', 'assets/vectorball/right.png', 50, 50);
-    game.load.spritesheet('N', 'assets/vectorball/up.png', 50, 50);
-    game.load.spritesheet('S', 'assets/vectorball/down.png', 50, 50);
-    game.load.spritesheet('W', 'assets/vectorball/left.png', 50, 50);
+    game.load.spritesheet('bounce', 'assets/vectorball/bouncer.png', 50, 50);
+    game.load.spritesheet('E', 'assets/vectorball/right2.png', 50, 50);
+    game.load.spritesheet('N', 'assets/vectorball/up2.png', 50, 50);
+    game.load.spritesheet('S', 'assets/vectorball/down2.png', 50, 50);
+    game.load.spritesheet('W', 'assets/vectorball/left2.png', 50, 50);
     game.load.image('END', 'assets/vectorball/end.png', 50, 50);
     game.load.image('+', 'assets/vectorball/add.jpg', 50, 50);
     game.load.image("slot", 'assets/vectorball/base.png', 50, 50);
     game.load.image('X', 'assets/vectorball/black.png', 50, 50);
+    game.load.spritesheet("complete", 'assets/vectorball/levelcomplete.png', 150, 550);
 }
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.physics.setBoundsToWorld();
     tiles = game.add.group();
+    line = game.add.graphics(0, 0);
     setUpLevel();
     var sprite = "end";
     setTiles();
+    //makeEndDoors();
+    //makeBtn();
+}
+function makeEndDoors() {
+    makeDoor(0xf4ea5a, game.width, game.height, 0);
+    makeDoor(0xf4c65a, 0, 0, (Math.PI));
+    makeDoor(0xf4a75a, game.width, 0, (3 * Math.PI / 2));
+    makeDoor(0xf4d95a, 0, game.height, (Math.PI / 2));
+}
+function makeDoor(color, px, py, rot) {
+    var door;
+    door = game.add.graphics(0, 0);
+    door.beginFill(color);
+    door.drawPolygon([new Phaser.Point(0, game.height), new Phaser.Point(game.width, 0), new Phaser.Point(game.width, game.height)]);
+    door.endFill();
+    door.pivot.x = 0;
+    door.pivot.y = game.height;
+    door.x = px;
+    door.y = py;
+    door.rotation = rot;
+    doors.push(door);
+}
+function closeDoor() {
+    makeEndDoors();
+    levelEnd = true;
 }
 function setUpLevel() {
+    makeBtn();
     console.log("SET UP");
     var row = 0;
     var col = 0;
@@ -100,6 +182,12 @@ function setUpLevel() {
         for (var i = 0; i < map[row].length; i++) {
             col = i;
             xpos = col * tileWidth;
+            if (map[row][i] === "R") {
+                game.add.sprite(xpos, ypos, "F", 0);
+                var newTile = game.add.sprite(xpos, ypos, "R", 0);
+                newTile.dir = "E";
+                tiles1.push(newTile);
+            }
             var newTile = game.add.sprite(xpos, ypos, map[row][i], 0);
             if (map[row][i] === 'X') {
                 holes.push(newTile);
@@ -123,7 +211,14 @@ function setUpLevel() {
     game.physics.enable(tiles2, Phaser.Physics.ARCADE)
     graphics = game.add.graphics(25, 25);
     graphics.lineStyle(10, 0xffd900, 1);
+    nope = game.add.sprite(startx, starty, "nope");
+    nope.bringToTop();
+    nope.visible = false;
     window.graphics = graphics;
+}
+function drawLine() {
+    line.lineStyle(10, 0xffd900, 1);
+    line.moveTo(100, 100).lineTo(300, 300).lineTo(200, 200);
 }
 function setTiles() {
     tiles2 = [];
@@ -133,28 +228,54 @@ function setTiles() {
         console.log("c: " + col + " r: " + row + " id: " + level.tiles[i]);
         var vt = VTile((i * tileWidth), 450, level.tiles[i]);
         tiles2.push(vt);
-        //tiles.add(vt);
     }
-    //console.log("Set tiles: " + JSON.stringify(tiles));
 }
 function VTile(x, y, val) {
-    var v = game.add.sprite(x, y, val);
+    var dir = val;
+    var v = game.add.sprite(x, y, dir);
+    v.origin = { x: x, y: y };
     v.inputEnabled = true;
+    v.canDrop = true;
     v.input.enableDrag();
-    v.input.enableSnap(tileWidth, tileWidth, true, true);
+    v.input.enableSnap(tileWidth, tileWidth, false, true);
     v.collidedWith = false;
     v.events.onDragStart.add(function () {
+        go = false;
+        currentTile = v;
         v.bringToTop();
         ball.bringToTop();
-    }, this)
+        nope.bringToTop();
+    }, this);
+    v.events.onDragStop.add(function () {
+        if (v.canDrop === false) {
+            v.x = v.origin.x;
+            v.y = v.origin.y;
+            nope.visible = false;
+        } else {
+            v.origin.x = v.x;
+            v.origin.y = v.y;
+        }
+        currentTile = null;
+        go = true;
+    }, this);
     v.dragFromCenter = true;
     v.dir = val;
+    v.animations.add('spring');
+    v.animations.currentAnim.onComplete.add(function () {
+    }, this);
     return v;
 }
-function collisionHandler() {
-    console.log("hole collide");
-}
 function update() {
+    if (collideTimer !== null) {
+        collideTimer--;
+        if (collideTimer <= 0) {
+            for (var i = 0; i < tiles2.length; i++) {
+                tiles2[i].collidedWith = false;
+            }
+            //alert("End Timer");
+            collideTimer = null;
+        }
+    }
     graphics.clear();
     graphics.lineStyle(10, 0xffd900, 1);
     if (nodes.length > 0) {
@@ -167,9 +288,10 @@ function update() {
         graphics.lineTo(ball.x, ball.y);
     }
     if (ball.body.velocity.x === 0 && ball.body.velocity.y === 0 && (ball.x !== startx || ball.y !== starty)) {
-        resetBall();
+        if (levelEnd === false) {
+            resetBall();
+        }
     }
-    // console.log(tiles2[0].x);
     if (tiles2.length > 0) {
         for (var i = 0; i < tiles2.length; i++) {
             adjustPos(tiles2[i]);
@@ -180,33 +302,104 @@ function update() {
                     ball.body.velocity.x -= speed;
                 } else if (tiles2[i].dir === "N") {
                     ball.body.velocity.y -= speed;
-                } else {
+                } else if (tiles2[i].dir === "S") {
                     ball.body.velocity.y += speed;
                 }
-                tiles2[i].collidedWith = true;
-                tiles2[i].frame = 1;
+                collideTimerSet(tiles2[i]);
+                tiles2[i].animations.play('spring', 30, false);
+                tiles2[i].frame = 0;
                 nodes.push({ x: tiles2[i].x, y: tiles2[i].y });
             }
         }
     }
-    if (!levelWin && game.physics.arcade.distanceBetween(ball, end) < 5) {
+    if (tiles1.length > 0) {
+        for (var i = 0; i < tiles1.length; i++) {
+            if (game.physics.arcade.distanceBetween(ball, tiles1[i]) < 5) {
+                //alert("add gravity");
+                if (tiles1[i].dir === "E") {
+                    ball.body.velocity.x = speed;
+                }
+                nodes.push({ x: tiles1[i].x, y: tiles1[i].y });
+            }
+        }
+    }
+    if (!levelWin && game.physics.arcade.distanceBetween(ball, end) < 15) {
         ball.body.velocity.x = 0;
         ball.body.velocity.y = 0;
-        alert("LEVEL COMPLETE!");
-        console.log("CLEAR AND RESET>>");
-        clearLevel();
-        reset();
+        closeDoor();
     }
     for (var i = 0; i < holes.length; i++) {
         if (game.physics.arcade.distanceBetween(ball, holes[i]) < 10) {
-            console.log("Going down the hole......... ");
             resetBall();
         }
     }
+    if (go === false) {
+        resetBall();
+    }
+    if (currentTile !== null) {
+        nope.x = currentTile.x;
+        nope.y = currentTile.y;
+    }
+    if (currentTile !== null) {
+        var collision = false;
+        for (var i = 0; i < tiles2.length; i++) {
+            if (currentTile !== tiles2[i]) {
+                if (game.physics.arcade.distanceBetween(currentTile, tiles2[i]) < 30) {
+                    collision = true;
+                }
+            }
+        }
+        if (collision) {
+            currentTile.canDrop = false;
+            nope.visible = true;
+        } else {
+            currentTile.canDrop = true;
+            nope.visible = false;
+        }
+    }
+    if (levelEnd) {
+        for (var i = 0; i < doors.length; i++) {
+            if (doorAngle < ((Math.PI / 2) - (Math.PI / 128))) {
+                doors[i].rotation -= (Math.PI / 128);
+            } else {
+                complete.bringToTop();
+                complete.visible = true;
+                nextbtn.bringToTop();
+                nextbtn.visible = true;
+            }
+        }
+        doorAngle += (Math.PI / 128);
+    }
+}
+function over() { };
+function out() { };
+function up() { };
+function nextLevel() {
+    clearLevel();
+    reset();
+}
+function makeBtn() {
+    console.log("MAKE BTN");
+    complete = game.add.sprite(0, 175, "complete");
+    complete.visible = false;
+    nextbtn = game.add.button(game.world.centerX - 75, game.world.centerY + 100, 'next', nextLevel, this, 1, 0, 2);
+    nextbtn.visible = false;
 }
 function clearLevel() {
-    console.log("clear");
     game.world.removeAll();
+}
+function hideNope() {
+    nope.visible = false;
+}
+function collideTimerSet(t) {
+    t.collidedWith = true;
+    collideTimer = 20;
+}
+function checkOverlap(t1, t2) {
+    var boundsA = t1.getBounds();
+    var boundsB = t2.getBounds();
+    var intersects = Phaser.Rectangle.intersects(boundsA, boundsB);
+    return intersects;
 }
 function adjustPos(tile) {
     if (tile.y <= 0) {
@@ -223,11 +416,9 @@ function adjustPos(tile) {
     }
 }
 function collisionCallback(ball, tile) {
-    console.log("callback");
     dx = 1;
 }
 function processCallback(ball, tile) {
-    console.log("process callback");
     return true;
 }
 function ballOut() {
@@ -240,18 +431,14 @@ function resetBall() {
     ball.y = starty;
     ball.body.velocity.y = 0;
     ball.body.velocity.x = 0;
+    ball.body.acceleration.x = 0;
+    ball.body.acceleration.y = 0;
     for (var i = 0; i < tiles2.length; i++) {
         tiles2[i].collidedWith = false;
         tiles2[i].frame = 0;
     }
 }
-function checkOverlap(spriteA, spriteB) {
-    var boundsA = spriteA.getBounds();
-    var boundsB = spriteB.getBounds();
-    return Phaser.Rectangle.intersects(boundsA, boundsB);
-}
 function reset() {
-    console.log("RESET");
     if (lvlnum < (levels.length - 1)) {
         lvlnum++;
     }
@@ -262,8 +449,12 @@ function reset() {
     })
     holes = [];
     nodes = [];
+    levelEnd = false;
+    doorAngle = 0;
     setUpLevel();
     setTiles();
+    doors = [];
+    //  makeEndDoors();
 }
 function destroyTiles() {
     tiles.forEach(function (tile) {
@@ -272,9 +463,13 @@ function destroyTiles() {
 }
 function render() {
     game.debug.text("DEMO! Drag", 180, 350);
-    game.debug.text("vector pads (below) ", 180, 365);
-    game.debug.text("onto course to move ", 180, 380);
-    game.debug.text("ball towards finish.", 180, 395);
+    // game.debug.text("vector pads (below) ", 180, 365);
+    // game.debug.text("onto course to move ", 180, 380);
+    // game.debug.text("ball towards finish.", 180, 395);
+    //game.debug.text("close Angle = " + doorAngle, 180, 395);
+    // if (currentTile === null) {
+    //     game.debug.text("current tile null", 180, 350);
+    // }
     // var ballB = ball.getBounds();
     // var plusB = plus.getBounds();
     // //  var d = game.physics.arcade.distanceBetween(ball, end);
@@ -283,5 +478,5 @@ function render() {
     // game.debug.text("intersecting?: " + plusB, 0, 120);
     // game.debug.text(Phaser.Rectangle.intersects(ballB, plusB), 0, 50);
     // game.debug.text("nodes: " + JSON.stringify(nodes), 10, 10);
-    //game.debug.text("vel x: " + ball.body.velocity.x + "vel y: " + ball.body.velocity.y, 550, 20)
+    game.debug.text("vel x: " + ball.body.velocity.x + "vel y: " + ball.body.velocity.y, 0, 20)
 }
